ca49c514fd985d75d9c08f9bb49001d805dca82e
Syed Sugail <suhailpno@gmail.com>
2025-07-16 17:52:44 +0530

    Fix dynamic import error by disabling minification

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: false,
     rollupOptions: {
       output: {
         manualChunks: {
