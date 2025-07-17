3b71f9576032e80fa69b2a3bcad01728957e7ab6
Syed Sugail <suhailpno@gmail.com>
2025-07-17 11:17:18 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'esbuild',
+    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
